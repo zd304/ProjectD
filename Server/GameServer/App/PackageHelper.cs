d067953b099d3a7393ea5975fa19fe5685d33ef3
zd304 <568892758@qq.com>
2019-10-05 22:46:03 +0800

    GameServer Handler

---
+++ Server/GameServer/App/PackageHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using ProtoBuf;
+using Photon.SocketServer;
+using System.Collections.Generic;
+
+namespace LobbyServer
+{
+    public static class PackageHelper
+    {
+        public static byte[] Serialize<T>(T instance)
+        {
+            byte[] bytes = null;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                Serializer.Serialize<T>(ms, instance);
+                bytes = new byte[ms.Position];
+                var fullBytes = ms.GetBuffer();
+                Array.Copy(fullBytes, bytes, bytes.Length);
+            }
+            return bytes;
+        }
+
+        public static T Desirialize<T>(byte[] bytes)
+        {
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                return Serializer.Deserialize<T>(ms);
+            }
+        }
+
+        public static void SetData(OperationResponse response, byte[] bytes)
+        {
+            Dictionary<byte, object> data = new Dictionary<byte, object>();
+            data[0] = bytes;
+            response.Parameters = data;
+        }
+    }
+}
