1a7c2f46d759bfdecc3c360d1781e6b067127ab0
zd304 <568892758@qq.com>
2019-10-20 01:03:47 +0800

    select race bg

--- Client/Assets/Scripts/UI/UISelectRace.cs
+++ Client/Assets/Scripts/UI/UISelectRace.cs
@@ -4,5 +4,57 @@ using UnityEngine;
 
 public class UISelectRace : MonoBehaviour
 {
+    private const int raceCount = 3;
+    private const int defaultRace = 2;
 
+    public Transform[] raceTranforms;
+    public GameObject[] raceBGs;
+
+    private int raceIndex = defaultRace;
+
+    public void OnClickLeftPage()
+    {
+        if (raceIndex > 1)
+        {
+            --raceIndex;
+        }
+        for (int i = 0; i < raceTranforms.Length; ++i)
+        {
+            Transform t = raceTranforms[i];
+            GameObject bg = raceBGs[i];
+            if (i == (raceIndex - 1))
+            {
+                t.localScale = Vector3.one;
+                bg.SetActive(true);
+            }
+            else
+            {
+                t.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+                bg.SetActive(false);
+            }
+        }
+    }
+
+    public void OnClickRightPage()
+    {
+        if (raceIndex < raceCount)
+        {
+            ++raceIndex;
+        }
+        for (int i = 0; i < raceTranforms.Length; ++i)
+        {
+            Transform t = raceTranforms[i];
+            GameObject bg = raceBGs[i];
+            if (i == (raceIndex - 1))
+            {
+                t.localScale = Vector3.one;
+                bg.SetActive(true);
+            }
+            else
+            {
+                t.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+                bg.SetActive(false);
+            }
+        }
+    }
 }
